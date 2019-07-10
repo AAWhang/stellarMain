2b053ec616f27dfc05e64156d6d2e973dd9d311c
Student <example@example.com>
2019-07-10 15:06:18 -0700

    safety commit

---
+++ src/Stellar-Trail.js
@@ -0,0 +1,233 @@
+export class Ship {
+
+  constructor(hp,money){
+    this.hp = hp;
+    this.fuel = 80000;
+    this.fuelcap = 100000;
+    this.money = money;
+    this.parts = 20;
+    this.materials = 0;
+    this.ammo = 0;
+    this.shield = 1000;
+    this.crew = [];
+    this.food = 20000;
+    this.foodcap = 20000;
+    this.speed = 0;
+    this.distance = 0;
+    this.spaceTime = 0;
+  }
+}
+
+
+export class Crew {
+    constructor(gender,age,occupation) {
+      this.gender = gender;
+      this.age = age;
+      this.occupation = occupation;
+      this.health = 300;
+    }
+}
+
+export class Planets {
+  constructor() {
+    this.environment = function(){
+      let die1 = Math.floor(Math.random() * 6 + 1);
+      if (die1 % 2 === 0) {
+        if (die1 === 2) {
+          this.lifeforms = "friendly";
+          this.trading = "true";
+        } else {
+          this.lifeforms = "hostile";
+          this.trading = "false";
+        }
+        return "Hospitable";
+      } else {
+        this.lifeforms = "none";
+        this.trading = "false";
+        return "Inhospitable";
+      }
+    };
+    this.materials = Math.floor(Math.random() * 10000 + 1);
+    this.gravity = Math.floor(Math.random() * 5000 + 1);
+  }
+}
+
+export class SpaceEvents {
+  gravityWell(fuel){
+    return fuel - 2000;
+  }
+
+  astroidBelt(ship){
+    let shieldReply = prompt("Use shields? y/n");
+    if (shieldReply === "y" && ship.shield >= 300) {
+      alert("Got past threat!");
+        ship.shield -= 300;
+      }else {
+        alert("Your ship takes damage!");
+        for (let i = 0; i < ship.crew.length; i++) {
+          ship.crew[i].health -= 30;
+        }
+        ship.hp -= 400;
+      }
+    return ship;
+  }
+
+  meteors(ship){
+    let shieldReply = prompt("Use shields? y/n");
+    if (shieldReply === "y" && ship.shield >= 100) {
+      alert("Got past threat!");
+        ship.shield -= 100;
+      }else {
+        alert("Your ship takes damage!");
+        for (let i = 0; i < ship.crew.length; i++) {
+          ship.crew[i].health -= 30;
+        }
+        ship.hp -= 200;
+      }
+    return ship;
+  }
+
+  spacePirates(ship){
+    let fightReply = prompt("Fight off Space Pirates? y/n");
+    if (fightReply === "y" && ship.ammo >= 40) {
+      alert("Fought off Space Pirates!");
+        ship.ammo -= 40;
+      }else {
+        alert("Space pirates attacked and plundered your ship!");
+        for (let i = 0; i < ship.crew.length; i++) {
+          ship.crew[i].health -= 30;
+        }
+        ship.money = ship.money / 2;
+        ship.materials = ship.materials / 2;
+        ship.food = ship.food / 2;
+        ship.hp -= 100;
+
+      }
+      return ship;
+  }
+
+  spaceVirus(crew){
+    let die1 = Math.floor(Math.random() * 4);
+      crew[die1].health -= 100;
+  }
+
+  spaceMadness(crew){
+
+    for (let i = 1; i < crew.length; i++) {
+      crew[i].health -= 40;
+    }
+  }
+
+  alienEncounter(crew){
+
+    let die1 = Math.floor(Math.random() * 6 + 1);
+    if (die1 % 2 === 0) {
+      alert("alien fucks you up");
+      for (let i = 0; i < crew.length; i++) {
+        crew[i].health -= 30;
+      }
+    } else {
+      alert("alien says hello!");
+    }
+  }
+
+  wormhole(distance){
+    let choice = prompt("Wormhole detected, enter? y/n?");
+    if (choice === "y") {
+      let die1 = Math.floor(Math.random() * 600 + 1);
+      return die1 * 1000;
+    } else {
+      return distance;
+    }
+  }
+
+  spaceStation(ship) {
+    alert("spaceStation");
+    ship.money += ship.materials * 10;
+    ship.materials = 0;
+    let buyFood = parseInt(prompt("How much food $10/ea"));
+    let buyParts = parseInt(prompt("How much Parts $100/ea"));
+    let buyFuel = parseInt(prompt("How much fuel $10/ea"));
+    let buyAmmo = parseInt(prompt("How much Ammo $50/ea"));
+    let total = (buyFood * 10) + (buyParts * 100) + (buyFuel * 10) + (buyAmmo * 50);
+    while (total > ship.money || ship.foodcap < ship.food + buyFood || ship.fuelCap < ship.fuel + buyFuel || buyFood === undefined || buyParts === undefined) {
+        alert("Get serious");
+        buyFood = parseInt(prompt("How much food $10/ea"));
+        buyParts = parseInt(prompt("How much Parts $100/ea"));
+        buyFuel = parseInt(prompt("How much fuel $10/ea"));
+        buyAmmo = parseInt(prompt("How much Ammo $50/ea"));
+        total = (buyFood * 50) + (buyParts * 100) + (buyFuel * 10) + (buyAmmo * 50);
+      }
+    let repaircost = 1000 - ship.hp;
+    let repairReply = prompt("Repairing will take" + repaircost / 100 + " ship parts, proceed?? y/n");
+    if (repairReply === "y") {
+      ship.hp = 1000;
+      ship.parts -= repaircost / 100;
+    }
+    let healReply = prompt("Heal crew for $1000? y/n");
+    if (healReply === "y") {
+      for (let i = 0; i < ship.crew.length; i++) {
+        ship.crew[i].health = 300;
+      }
+    }
+    let shieldReply = prompt("Fully recharge Shield for $10,000? y/n");
+    if (shieldReply === "y") {
+      ship.shield = 1000;
+    }
+    ship.fuel += buyFuel;
+    ship.parts += buyParts;
+    ship.food += buyFood;
+    ship.ammo += buyAmmo;
+    ship.money -= total;
+    return ship;
+  }
+
+
+  ghostStation(ship) {
+    let choice = prompt("Would you like to explore Ghost Station? y/n?");
+    if (choice === "y") {
+    let die1 = Math.floor(Math.random() * 2);
+    if (die1 === 1) {
+      ship.fuel += 5000;
+      console.log(ship.fuel);
+    }
+    die1 = Math.floor(Math.random() * 2);
+    if (die1 === 1) {
+      ship.money += 5000;
+      console.log(ship.money);
+    }
+    die1 = Math.floor(Math.random() * 2);
+    if (die1 === 1) {
+      ship.parts += 5000;
+      console.log(ship.parts);
+    }
+    die1 = Math.floor(Math.random() * 2);
+    if (die1 === 1) {
+      ship.materials += 5000;
+      console.log(ship.materials);
+    }
+    die1 = Math.floor(Math.random() * 2);
+    if (die1 === 1) {
+      ship.weapons += 5000;
+      console.log(ship.weapons);
+    }
+    die1 = Math.floor(Math.random() * 2);
+    if (die1 === 1) {
+      ship.food += 5000;
+      console.log(ship.food);
+    }
+    die1 = Math.floor(Math.random() * 2);
+    die1 = 1;
+    if (die1 === 1) {
+      for (let i = 0; i < ship.crew.length; i++) {
+        ship.crew[i].health -= 30;
+        alert(ship.crew[i].health);
+      }
+    }
+  } else {
+    alert("You left the station alone");
+  }
+
+
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import {Ship,Crew,Planets,SpaceEvents} from "./Stellar-Trail.js";
 
 $(document).ready(function() {
   $("#gameSelector").hide();
   $("#envoy").hide();
   $("#colonize").hide();
   $("#resultScreen").hide();
-  });
+
 
   $("#start").click(function() {
     $("#home").hide();
@@ -19,7 +20,212 @@ $(document).ready(function() {
   $("#game1").click(function() {
     $("#gameSelector").hide();
     $("#envoy").show();
+    let missionEnvoy = new Ship(500,1000000);
+    let spacemonths = 0;
+    let spaceyears = 0;
+    let planetArray = [];
+    let crew1 = new Crew("male",35,"Snitch");
+    let crew2 = new Crew("male",25,"Pianist");
+    let crew3 = new Crew("female",99,"Witch");
+    let crew4 = new Crew("female",17,"Minor");
+    let whyGod = new SpaceEvents;
+    missionEnvoy.crew = [crew1,crew2,crew3,crew4];
+    let pause = 1;
+    for (let p = 0; p <= 20; p++) {
+      planetArray[p] = new Planets();
+    }
+
+    let timer = setInterval(logM, 1000);
+    let timer2 = setInterval(logN, 50);
+
+    function logM() {
+      missionEnvoy.spaceTime += 1;
+      spacemonths += 1;
+      $("#months").html("Months: " + spacemonths);
+      $("#years").html("Years: " + spaceyears);
+      if (spacemonths >= 12) {
+        spacemonths = 0;
+        spaceyears += 1;
+      }
+      randomSpaceEvents(missionEnvoy.spaceTime);
+    }
+
+
+
+    function logN() {
+      missionEnvoy.distance += 50;
+      missionEnvoy.fuel -= 20;
+      missionEnvoy.food -= 5;
+      // let die1 = Math.floor(Math.random() * 4);
+      // missionEnvoy.crew[die1].health -= 1;
+      $("#shipHp").html("<progress id='shipHp' value=" + Math.floor(missionEnvoy.hp / 10) + " max='100'></progress>");
+      $("#fuel").html("<progress id='fuel' value=" + Math.floor(missionEnvoy.fuel * 100 / missionEnvoy.fuelcap) + " max='100'></progress>");
+      $("#food").html("<progress id='food' value=" + Math.floor(missionEnvoy.food * 100 / missionEnvoy.foodcap) + " max='100'></progress>");
+      $("#shield").html("<progress id='shield' value=" + Math.floor(missionEnvoy.shield / 10) + " max='100'></progress>");
+      $("#money").html("ship money: " + missionEnvoy.money);
+      $("#shipWeapons").html("ship ammo: " + missionEnvoy.ammo);
+      $("#travelDistance").html("Distance: " + missionEnvoy.distance);
+      $("#distanceTraveled").html("<progress id='distanceTraveled' value=" + Math.floor(missionEnvoy.distance * 100 / 600000) + " max='100'></progress>");
+
+      $("#shipOre").html("ship materials: " + missionEnvoy.materials);
+      $("#shipParts").html("ship parts: " + missionEnvoy.parts);
+      for (let i = 0; i < missionEnvoy.crew.length; i++) {
+        $("#e0").append("crew" + i + "health: " + missionEnvoy.crew[i].health);
+      }
+      spaceHappenings(missionEnvoy.distance);
+      deathCheck();
+      gameOverCheck();
+      winCheck();
+    }
+
+    function spaceHappenings(gamedist) {
+      if (gamedist === 100000) {
+        whyGod.spaceStation(missionEnvoy);
+      }
+      if (gamedist === 120000) {
+        whyGod.spaceStation(missionEnvoy);
+      }
+      if (gamedist === 240000) {
+        whyGod.spaceStation(missionEnvoy);
+      }
+      if (gamedist === 360000) {
+        whyGod.spaceStation(missionEnvoy);
+      }
+      if (gamedist === 600000) {
+        whyGod.spaceStation(missionEnvoy);
+      }
+
+      if (gamedist % 30000 === 0) {
+        planetEvents();
+      }
+
+      if (gamedist % 70000 === 0) {
+        alert("astroidBelt");
+        missionEnvoy = whyGod.astroidBelt(missionEnvoy);
+      }
+
+    }
+
+    function randomSpaceEvents(months) {
+      let die1 = Math.floor(Math.random() * 12 + 1);
+      if (months % 4 === 0){
+        if (die1 === 3 || die1 === 6 || die1 === 9){
+          thingsHappen();
+        }
+      }
+    }
+
+    function thingsHappen(){
+      let die1 = Math.floor(Math.random() * 10 + 1);
+      if (die1 === 1){
+        alert("gravityWell");
+        missionEnvoy.fuel = whyGod.gravityWell(missionEnvoy.fuel);
+      }
+      if (die1 === 2){
+        alert("astroidBelt");
+        missionEnvoy = whyGod.astroidBelt(missionEnvoy);
+      }
+      if (die1 === 3){
+        alert("meteors");
+        missionEnvoy = whyGod.meteors(missionEnvoy);
+      }
+      if (die1 === 4){
+        alert("spacePirates");
+        missionEnvoy = whyGod.spacePirates(missionEnvoy);
+      }
+      if (die1 === 5){
+
+        alert("spaceVirus");
+        whyGod.spaceVirus(missionEnvoy.crew);
+      }
+      if (die1 === 6){
+        alert("spaceMadness");
+        whyGod.spaceMadness(missionEnvoy.crew);
+      }
+      if (die1 === 7){
+        alert("you find alien!");
+        whyGod.alienEncounter(missionEnvoy.crew);
+      }
+      if (die1 === 8){
+        alert("wormhole");
+        missionEnvoy.distance = whyGod.wormhole(missionEnvoy.distance);
+      }
+      if (die1 === 9){
+        whyGod.ghostStation(missionEnvoy);
+      }
+      if (die1 === 10){
+        whyGod.spaceStation(missionEnvoy);
+      }
+    }
+
+    function planetEvents(){
+      let die1 = Math.floor(Math.random() * 20 + 1);
+      alert("You have found a " + planetArray[die1].environment() + " planet");
+      let choice = prompt("Would you like to explore? y/n?");
+      if (choice === "y") {
+        if(planetArray[die1].environment() === "Hospitable") {
+          if (planetArray[die1].lifeforms === "friendly") {
+            alert("You found friendly aliens!");
+            missionEnvoy.food += 2000;
+            missionEnvoy.fuel += 3000;
+            missionEnvoy.materials +=  planetArray[die1].materials;
+          } else {
+            alert("You find hostile aliens! They attack!");
+            let fightReply = prompt("Fight off aliens? y/n");
+            if (fightReply === "y" && missionEnvoy.ammo >= 10) {
+              alert("Fought off aliens");
+              missionEnvoy.ammo -= 10;
+              missionEnvoy.food += 3000;
+              missionEnvoy.fuel += 3000;
+              missionEnvoy.materials +=  planetArray[die1].materials;
+            } else {
+              alert("Aliens attacked your crew!");
+              for (let i = 0; i < missionEnvoy.crew.length; i++) {
+                missionEnvoy.crew[i].health -= 30;
+              }
+            }
+          }
+          missionEnvoy.fuel -= planetArray[die1].gravity;
+          alert("cost to escape planet: " + planetArray[die1].gravity);
+        } else {
+          missionEnvoy.materials +=  planetArray[die1].materials;
+          missionEnvoy.fuel -= planetArray[die1].gravity;
+          alert("cost to escape planet: " + planetArray[die1].gravity);
+        }
+      }
+    }
+
+    function gameOverCheck(){
+      if (missionEnvoy.hp <= 0 || missionEnvoy.fuel <= 0 || missionEnvoy.crew.length === 0 || missionEnvoy.food <= 0) {
+        alert("game over!");
+        clearInterval(timer);
+        clearInterval(timer2);
+      }
+    }
+
+    function deathCheck(){
+      for (let i = 0; i < missionEnvoy.crew.length; i++) {
+        if (missionEnvoy.crew[i].health <= 0) {
+          missionEnvoy.crew.splice(i,1);
+        }
+      }
+    }
+
+    function winCheck(){
+      if (missionEnvoy.distance >= 600000) {
+        alert("You are Oregon Space!");
+        clearInterval(timer);
+        clearInterval(timer2);
+      }
+    }
   });
+
+
+
+
+
+
+
 
   $("#game2").click(function() {
     $("#gameSelector").hide();
@@ -45,3 +251,26 @@ $(document).ready(function() {
     $("#resultScreen").hide();
     $("#home").show();
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
